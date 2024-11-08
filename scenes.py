ead559c8e351b801787852f594b95c0ed248c7b4
SohamVaishnav <soham.vaishnav@research.iiit.ac.in>
2024-11-09 00:13:08 +0530

    defined the essential functions; timer is remaining

--- scenes.py
+++ scenes.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import soundfile as sf
 
 from utils import *
 
@@ -8,12 +9,30 @@ class Movement(object):
     Movement class
     '''
     def __init__(self, color: tuple, position, screen: Stage):
+        '''
+        Movement class constructor
+
+        Parameters:
+            color (tuple): color of the object
+            position (list): initial position of the object
+            screen (Stage): stage object 
+        '''
         self.object_creator = Item(color, position)
+        self.position = position
         self.screen = screen
         self.object_history = []
         pass
 
     def createObject(self, mode: str, radius: int = None, arrow_dims: list = None, plus_dims: list = None):
+        '''
+        Create an object
+
+        Parameters:
+            mode (str): mode of the object
+            radius (int): radius of the object
+            arrow_dims (list): dimensions of the arrow
+            plus_dims (list): dimensions of the plus
+        '''
         if (mode == 'dot'):
             self.object_creator.dot(radius)
         elif (mode == 'arrow'):
@@ -27,14 +46,25 @@ class Movement(object):
         pass
     
     def move(self, direction: str, speed: int):
-        if (direction == 'left'):
-            pass
-        elif (direction == 'right'):
-            pass
-        else:
-            print("Invalid direction!")
+        '''
+        Move the object
+
+        Parameters:
+            direction (str): direction of the movement
+            speed (int): speed of the movement
+        '''
         self.dir = direction
         self.speed = speed
+        if (direction == 'left'):
+            direction = -1
+            self.start_pos_x = self.position[0]
+            self.start_pos_y = self.position[1]
+        elif (direction == 'right'):
+            direction = 1
+            self.start_pos_x = self.position[0]
+            self.start_pos_y = self.position[1]
+        else:
+            print("Invalid direction!")
         pass
 
 class Direction(object):
@@ -42,46 +72,100 @@ class Direction(object):
     Direction class
     '''
     def __init__(self, position: list, color: tuple, screen: Stage):
+        '''
+        Direction class constructor
+
+        Parameters:
+            position (list): position of the object
+            color (tuple): color of the object
+            screen (Stage): stage object
+        '''
         self.position = position
         self.object_creator = Item(color, position)
         self.screen = screen
         pass
 
     def createArrow(self, arrow_dims: list):
+        '''
+        Create an arrow
+
+        Parameters:
+            arrow_dims (list): dimensions of the arrow
+        '''
         self.arrow = self.object_creator.arrow(arrow_dims)
-        pass
+        return self.arrow
         
 class Sound(object):
     '''
     Sound class
     '''
     def __init__(self, screen: Stage):
+        '''
+        Sound class constructor
+
+        Parameters:
+            screen (Stage): stage object
+        '''
         self.object_creator = Item()
         self.screen = screen
         pass
 
     def createBeep(self, frequency: int, duration: int, aplitude: int):
+        '''
+        Create a beep sound
+
+        Parameters:
+            frequency (int): frequency of the beep
+            duration (int): duration of the beep
+            amplitude (int): amplitude of the beep
+        '''
+        self.freq = frequency
+        self.dur = duration
+        self.amp = aplitude
         self.sound = self.object_creator.beep(frequency, duration, aplitude)
+        sf.write(f"beep_f{frequency}_dur{duration}_amp{aplitude}.mp4", self.sound, 44100, format='WAV', subtype='PCM_16')
         pass
 
-    def play(self, ear: str):
+    def play(self, ear: str, beep_file_name: str):
+        '''
+        Plays the sound
+
+        Parameters:
+            ear (str): ear to play the sound
+            beep_file_name (str): name of the beep file
+        '''
+        beep = pygame.mixer.Sound(beep_file_name)
+        channel = pygame.mixer.Channel(0)
         if (ear == 'left'):
-            pass
+            channel.set_volume(1.0, 0.0)
         elif (ear == 'right'):
-            pass
+            channel.set_volume(0.0, 1.0)
         else:
             print("Invalid ear!")
-        pass
+        channel.play(beep)
 
 class Timer(object):
     '''
     Timer class
     '''
     def __init__(self, screen: Stage):
+        '''
+        Timer class constructor
+
+        Parameters:
+            screen (Stage): stage object
+        '''
         self.screen = screen
         pass
 
     def draw_timer_ring(self, time_elapsed, max_time):
+        '''
+        Draw a timer ring
+
+        Parameters:
+            time_elapsed (int): time elapsed
+            max_time (int): maximum time
+        '''
         proportion = time_elapsed / max_time
         angle = 2 * 3.14 * proportion
         # self.timer = pygame.draw.arc(self.screen, (0, 255 - int(255 * proportion), 0), (700, 10, 50, 50), 0, angle, 5)
--- utils.py
+++ utils.py
@@ -1,11 +1,20 @@
 import numpy as np
 import pygame
+import sys
 
 class Stage(object):
     '''
     Stage class 
     '''
     def __init__(self, color: tuple, width = 100, height = 100):
+        '''
+        Stage class constructor
+
+        Parameters:
+            color (tuple): color of the stage
+            width (int): width of the stage
+            height (int): height of the stage 
+        '''
         self.color = color
         self.width = width
         self.height = height
@@ -13,22 +22,38 @@ class Stage(object):
         pass
 
     def setColor(self, color):
+        ''' 
+        Set the color of the stage
+
+        Parameters:
+            color (tuple): color of the stage
+        '''
         self.color = color
         return None
 
     def createWindow(self, width, height, caption: str):
+        '''
+        Create a window for the stage
+
+        Parameters:
+            width (int): width of the window
+            height (int): height of the window
+            caption (str): caption of the window
+        '''
         self.width = width
         self.height = height
         pygame.init()
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption(caption)
-        self.screen.fill(self.color)
-        pygame.display.update()
-        self.running = True
+        self.clock = pygame.time.Clock()
         pass
 
     def quit(self):
+        '''
+        Quit the stage
+        '''
         pygame.quit()
+        sys.exit()
         pass
 
 class Item(object):
@@ -36,31 +61,74 @@ class Item(object):
     Item class
     '''
     def __init__(self, color: tuple = None, position = None):
+        '''
+        Item class constructor
+
+        Parameters:
+            color (tuple): color of the item
+            position (tuple): position of the item
+        '''
         self.color = color
         self.position = position
         self.object = None
         pass
 
     def dot(self, radius):
-        dot = None
+        '''
+        Create a dot item
+
+        Parameters:
+            radius (int): radius of the dot
+        '''
+        dot = pygame.Surface((radius, radius), pygame.SRCALPHA)
+        pygame.draw.circle(dot, self.color, (radius//2, radius//2), radius//2)
         self.object = dot
         return dot
 
     def arrow(self, arrow_dims: list):
-        arrow = None
+        '''
+        Create an arrow item
+
+        Parameters:
+            arrow_dims (list): dimensions of the arrow
+        '''
         self.length = arrow_dims[0]
         self.width = arrow_dims[1]
         self.head_height = arrow_dims[2]
+        arrow = pygame.Surface((self.length, self.width), pygame.SRCALPHA)
+        pygame.draw.polygon(arrow, self.color, [(0, 0), (self.length, self.width//2), (0, self.width)])
+        pygame.draw.polygon(arrow, self.color, [(self.length - self.head_height, 0), (self.length, self.width//2), (self.length - self.head_height, self.width)])
         self.object = arrow
         return arrow
 
     def plus(self, plus_dims: list):
-        plus = None
+        '''
+        Create a plus item
+
+        Parameters:
+            plus_dims (list): dimensions of the plus
+        '''
+        plus = pygame.Surface((plus_dims[0], plus_dims[1]), pygame.SRCALPHA)
+        pygame.draw.rect(plus, self.color, (0, plus_dims[1]//3, plus_dims[0], plus_dims[1]//3))
+        pygame.draw.rect(plus, self.color, (plus_dims[0]//3, 0, plus_dims[0]//3, plus_dims[1]))
         self.object = plus
         return plus
     
     def beep(self, frequency: int, duration: int, aplitude: int):
+        '''
+        Create a beep item. Note that the sample rate is maintained at 44100 Hz.
+
+        Parameters:
+            frequency (int): frequency of the beep
+            duration (int): duration of the beep
+            aplitude (int): aplitude of the beep
+        '''
         beep = None
-        self.sound = beep
+        sample_rate = 44100
+        t = np.linspace(0, duration, sample_rate * duration)
+        beep = aplitude * np.sin(2 * np.pi * frequency*t)
+
+        beep = beep.astype(np.int16)
+        self.object = beep
         return beep
 
