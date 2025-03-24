f9f5718ffbfba90dc06ea74ff3380cd7da29f314
Abhinav Raundhal <abhinav.raundhal@students.iiit.ac.in>
2025-03-25 03:02:15 +0530

    og stroop updates

--- original_stroop/audio_processing/transcriber.py
+++ original_stroop/audio_processing/transcriber.py
@@ -2,6 +2,10 @@ import whisper
 import sounddevice as sd
 import numpy as np
 import scipy.io.wavfile as wav
+import os
+import warnings
+warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
+
 
 def record_audio(filename="user_audio.wav", duration=5, samplerate=44100):
     audio_data = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1, dtype=np.int16)
@@ -10,14 +14,21 @@ def record_audio(filename="user_audio.wav", duration=5, samplerate=44100):
 
 def transcribe_audio_files(files: list):
     # files is a list of file paths
+    print("Loading model...")
+    # model can be "base", "tiny", "medium", "large", "turbo"
     model = whisper.load_model("base")
     results = []
+    print("Transcribing audio files...")
     for file in files:
-        result = model.transcribe(file)
+        result = model.transcribe(file, language="english")
         results.append(result["text"])
     return results
 
-# if __name__ == "__main__":
-#     record_audio(duration=5)
-#     transcription = transcribe_audio()
-#     print("\nTranscribed Text:", transcription)
+if __name__ == "__main__":
+    path_to_files = "../audio_outputs_soham"
+    files = os.listdir(path_to_files)
+    files = [os.path.join(path_to_files, file) for file in files]
+    files = sorted(files)
+    results = transcribe_audio_files(files)
+    for i, result in enumerate(results):
+        print(f"File {files[i]}: {result}")
--- original_stroop/main.py
+++ original_stroop/main.py
@@ -1,8 +1,9 @@
 import pygame
+import os
 import random
 import time
 import csv
-from original_stroop.audio_processing.transcriber import *
+from audio_processing.transcriber import *
 import argparse
 import threading
 
@@ -15,8 +16,8 @@ parser.add_argument('--num_congruent', type=int, default=12, help='Number of con
 parser.add_argument('--num_conflict', type=int, default=12, help='Number of conflict trials')
 parser.add_argument('--screen_width', type=int, default=1700, help='Width of the screen')
 parser.add_argument('--screen_height', type=int, default=900, help='Height of the screen')
-parser.add_argument('--stimulus_duration', type=float, default=1.5, help='Duration of the stimulus')
-parser.add_argument('--inter_stimulus_interval', type=float, default=0.5, help='Interval between stimuli')
+parser.add_argument('--stimulus_duration', type=float, default=1, help='Duration of the stimulus')
+parser.add_argument('--inter_stimulus_interval', type=float, default=0.25, help='Interval between stimuli')
 parser.add_argument('--background_color', type=str, default="black", help='Background color of the screen')
 parser.add_argument('--neutral_words', type=str, default="./neutral_words.txt", help='File containing neutral words')
 args = parser.parse_args()
@@ -63,41 +64,7 @@ with open(config["neutral_words"], "r") as f:
     for line in f:
         neutral_words_array.append(tuple(line.strip().split(",")))
 
-print(neutral_words_array)
-
-# Data storage
-results = []
-
-
-# Function to generate a trial
-def generate_trial():
-    word = random.choice(COLOR_NAMES)
-    condition = random.choice(["congruent", "conflict"])
-
-    if condition == "congruent":
-        color_name = word
-    elif condition == "conflict":
-        color_name = random.choice([c for c in COLOR_NAMES if c != word])
-    else:
-        print("Something is worng with generate_trail functơn in base_test.py")
-    # else:  # Neutral condition
-    #     word = random.choice(neutral_words_array)
-    #     color_name = random.choice(COLOR_NAMES)
-
-    return word, COLORS[color_name], condition
-
-def generate_neutral_trial():
-    word = random.choice(neutral_words_array)
-    color_name = random.choice(COLOR_NAMES)
-    return word, COLORS[color_name], "neutral"
-
-# Function to display text
-# def display_text(text, color):
-#     screen.fill(background_color)
-#     text_surface = FONT.render(text, True, color)
-#     text_rect = text_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-#     screen.blit(text_surface, text_rect)
-#     pygame.display.flip()
+# print(neutral_words_array)
 
 def display_text_until_space(text, color):
     screen.fill(background_color)
@@ -123,109 +90,88 @@ def display_text_for_duration(text, color, duration):
     time.sleep(duration)
 
 
-# Function to run a single trial
-# def run_trial(case = "non-neutral"):
-#     if case == "neutral":
-#         word, color, condition = generate_neutral_trial()
-#     else:
-#         word, color, condition = generate_trial()
-#     display_text(word, color)
-
-#     start_time = time.time()
-#     reaction = None
-#     response_time = None
-
-#     while True:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 return None
-
-#             if event.type == pygame.KEYDOWN:
-#                 if event.key == pygame.K_r:
-#                     reaction = "RED"
-#                 elif event.key == pygame.K_g:
-#                     reaction = "GREEN"
-#                 elif event.key == pygame.K_b:
-#                     reaction = "BLUE"
-#                 elif event.key == pygame.K_y:
-#                     reaction = "YELLOW"
-
-#                 if reaction:
-#                     response_time = time.time() - start_time
-#                     break
-
-#         # If time exceeds stimulus duration, break
-#         if time.time() - start_time > stimulus_duration:
-#             break
-
-#     # Determine correctness
-#     correct = (
-#         1
-#         if reaction
-#         and reaction == [key for key, val in COLORS.items() if val == color][0]
-#         else 0
-#     )
-#     results.append(
-#         [word, condition, reaction, response_time if response_time else "N/A", correct]
-#     )
-
-#     # Inter-stimulus interval
-#     screen.fill(background_color)
-#     pygame.display.flip()
-#     time.sleep(inter_stimulus_interval)
-
-def run_neutral(word_color_pair: tuple, filename: str):
-
-    def display_and_record():
+def run_case(word_color_pair: tuple, filename: str):
+    def display():
         display_text_for_duration(word_color_pair[0], word_color_pair[1], stimulus_duration)
+
+    def record():
         record_audio(filename, duration=stimulus_duration + 0.25 * inter_stimulus_interval)
 
-    # Start display and recording in a separate thread
-    thread = threading.Thread(target=display_and_record)
-    thread.start()
-    thread.join()
+    # Start two separate threads
+    display_thread = threading.Thread(target=display)
+    record_thread = threading.Thread(target=record)
+
+    display_thread.start()
+    record_thread.start()
+
+    # Wait for both to complete
+    display_thread.join()
+    record_thread.join()
 
     # Inter-stimulus interval
     screen.fill(background_color)
     pygame.display.flip()
     time.sleep(inter_stimulus_interval * 0.75)
-    print(f"time: {time.time() - start_time}")
 
+def play_neutral(usr_folder_path: str):
+    neutral_count = 0
+    display_text_for_duration("Say the WORD", text_color, 1.5)
+    while neutral_count < num_neutral:
+        file_name = f"{usr_folder_path}/neutral_{neutral_count}_word.wav"
+        run_case(neutral_words_array[neutral_count], file_name)
+        neutral_count += 1
+    neutral_count = 0
+    display_text_for_duration("Say the COLOR", text_color, 1.5)
+    while neutral_count < num_neutral:
+        file_name = f"{usr_folder_path}/neutral_{neutral_count}_color.wav"
+        run_case(neutral_words_array[neutral_count], file_name)
+        neutral_count += 1
+
+def play_congruent(usr_folder_path: str):
+    COLOR_NAMES_CONGRUENT = COLOR_NAMES * 3
+    COLOR_NAMES_CONGRUENT = np.random.permutation(COLOR_NAMES_CONGRUENT)
+    congruent_count = 0
+    display_text_for_duration("Say the WORD", text_color, 1.5)
+    while congruent_count < num_congruent:
+        file_name = f"{usr_folder_path}/congruent_{congruent_count}_word.wav"
+        run_case((COLOR_NAMES_CONGRUENT[congruent_count], COLORS[COLOR_NAMES_CONGRUENT[congruent_count]]), file_name)
+        congruent_count += 1
+    congruent_count = 0
+    display_text_for_duration("Say the COLOR", text_color, 1.5)
+    while congruent_count < num_congruent:
+        file_name = f"{usr_folder_path}/congruent_{congruent_count}_color.wav"
+        run_case((COLOR_NAMES_CONGRUENT[congruent_count], COLORS[COLOR_NAMES_CONGRUENT[congruent_count]]), file_name)
+        congruent_count += 1
+
+def play_conflict(usr_folder_path: str):
+    COLOR_NAMES_CONFLICT = []
+    for color in COLOR_NAMES:
+        for c in COLOR_NAMES:
+            if c != color:
+                COLOR_NAMES_CONFLICT.append((color, c))
+    COLOR_NAMES_CONFLICT = np.random.permutation(COLOR_NAMES_CONFLICT)
+    conflict_count = 0
+    display_text_for_duration("Say the WORD", text_color, 1.5)
+    while conflict_count < num_conflict:
+        file_name = f"{usr_folder_path}/conflict_{conflict_count}_word.wav"
+        run_case(COLOR_NAMES_CONFLICT[conflict_count], file_name)
+        conflict_count += 1
+    conflict_count = 0
+    display_text_for_duration("Say the COLOR", text_color, 1.5)
+    while conflict_count < num_conflict:
+        file_name = f"{usr_folder_path}/conflict_{conflict_count}_color.wav"
+        run_case(COLOR_NAMES_CONFLICT[conflict_count], file_name)
+        conflict_count += 1
 
 # Main function to run the experiment
 def main():
+    usr_folder_path = "./audio_outputs_abhinav"
+    os.makedirs(usr_folder_path, exist_ok=True)
+    play_neutral(usr_folder_path)
+    play_congruent(usr_folder_path)
+    play_conflict(usr_folder_path)
+    pygame.quit()
 
-    neutral_count = 0
-    display_text_for_duration("Say the WORD", text_color, 1)
-    while neutral_count < num_neutral:
-        file_name = f"neutral_{neutral_count}_word.wav"
-        run_neutral(neutral_words_array[neutral_count], file_name)
-        neutral_count += 1
-    # neutral_count = 0
-    # display_text_for_duration("Say the COLOR", text_color, 1)
-    # while neutral_count < num_neutral:
-    #     file_name = f"neutral_{neutral_count}_color.wav"
-    #     run_neutral(neutral_words_array[neutral_count], file_name)
-    #     neutral_count += 1
-
-    # while running and trial_count < NUM_TRIALS:
-    #     run_trial()
-    #     trial_count += 1
-
-    # # Save results to CSV
-    # with open("stroop_results.csv", "w", newline="") as f:
-    #     writer = csv.writer(f)
-    #     writer.writerow(
-    #         ["Word", "Condition", "Response", "Reaction Time (s)", "Correct"]
-    #     )
-    #     writer.writerows(results)
-
-    # print("Experiment complete! Results saved to stroop_results.csv")
-    # pygame.quit()
-
-
-# Run the experiment
 if __name__ == "__main__":
     main()
 
